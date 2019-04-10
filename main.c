dfe8b153484b0a601c4bc21b75c1baedf5bc9eec
JanSnoeijsEPFL <jan.snoeijs@epfl.ch>
2019-04-10 10:47:10 +0200

    .

--- main.c
+++ main.c
@@ -40,6 +40,7 @@ int main() {
     }
     char a = SetBit(0x0,0,1);
     printf("a %c\n", a);
+    usleep(ALT_MICROSECS_IN_A_SEC);
     int32_t* words = calloc(NBWORDS, sizeof(int32_t));
     parse_weights("keras_param_3class_30e_5bits_onlysign_modifiedHardSigm.txt", &words);
     //concat_words(&words, words);
