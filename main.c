fb5b94c68bd2b24afb966cafead06152e3169b3e
JanSnoeijsEPFL <jan.snoeijs@epfl.ch>
2019-04-10 09:16:41 +0200

    .

--- main.c
+++ main.c
@@ -84,20 +84,20 @@ int main() {
     free(words);
     printf("writing to accelerator\n");
     read_accelerator(address+4);
-    usleep(ALT_MICROSECS_IN_A_SEC*1000);
+    usleep(ALT_MICROSECS_IN_A_SEC*100);
     printf("writing to accelerator\n");
     read_accelerator(address+1);
-    usleep(ALT_MICROSECS_IN_A_SEC*1000);
+    usleep(ALT_MICROSECS_IN_A_SEC*100);
     printf("writing to accelerator\n");
 	write_accelerator(address);
-    usleep(ALT_MICROSECS_IN_A_SEC*1000);
+    usleep(ALT_MICROSECS_IN_A_SEC*100);
     printf("writing to accelerator\n");
 	read_accelerator(address+4);
-    usleep(ALT_MICROSECS_IN_A_SEC*1000);
+    usleep(ALT_MICROSECS_IN_A_SEC*100);
     printf("writing to accelerator\n");
 	read_accelerator(address+1);
 	//printf("observe signal tap\n");
-	usleep(ALT_MICROSECS_IN_A_SEC * 1000);
+	usleep(ALT_MICROSECS_IN_A_SEC * 100);
 
 
     munmap_peripherals();
