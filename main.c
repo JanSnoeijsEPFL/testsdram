6ea2aef76aec0bcad0f6136ffe7a59b4356b6fc4
JanSnoeijsEPFL <jan.snoeijs@epfl.ch>
2019-05-14 16:29:40 +0200

    .

--- main.c
+++ main.c
@@ -31,21 +31,26 @@ int main() {
     free(words);
 
 
-    int32_t* xdata = calloc(RT_DATA_CHUNK_SIZE, sizeof(int32_t));
+    int32_t* xdata = NULL;
+    int32_t* DEBUG_data_words = NULL;
+    int32_t* DEBUG_data_maxp = NULL;
+    int32_t* DEBUG_data_gru = NULL;
+
+    xdata = calloc(RT_DATA_CHUNK_SIZE, sizeof(int32_t));
     if (xdata == NULL)
     	return EXIT_FAILURE;
 
-    int32_t* DEBUG_data_words = calloc(26*20, sizeof(int32_t));
-    if (DEBUG_data_words == NULL)
-		return EXIT_FAILURE;
+   // DEBUG_data_words = calloc(26*20, sizeof(int32_t));
+   // if (DEBUG_data_words == NULL)
+	//	return EXIT_FAILURE;
 
-	int32_t* DEBUG_data_maxp = calloc(1078, sizeof(int32_t));
-	if (DEBUG_data_words == NULL)
-		return EXIT_FAILURE;
+	// DEBUG_data_maxp = calloc(1078, sizeof(int32_t));
+	//if (DEBUG_data_words == NULL)
+	//	return EXIT_FAILURE;
 
-	int32_t* DEBUG_data_gru = calloc(400, sizeof(int32_t));
-	if (DEBUG_data_gru == NULL)
-		return EXIT_FAILURE;
+	// DEBUG_data_gru = calloc(400, sizeof(int32_t));
+	//if (DEBUG_data_gru == NULL)
+	//	return EXIT_FAILURE;
 
 
     uint8_t timesteps = 0;
@@ -57,35 +62,48 @@ int main() {
 	//FIRST algorithm iteration
 	parse_rtdata("RT_datastream.txt", &xdata, 0);
 	xocram_fill_RT(xocram, xdata);
-
+	free(xdata);
     for (timesteps=0; timesteps < 10; i++){
     	printf("iteration number %d\n", i);
 
 		write_accelerator(0, 3); // xocram B port in FPGA mode + trigger accelerator
 		write_accelerator(0, 2); //  deassert trigger
+		xdata = calloc(RT_DATA_CHUNK_SIZE, sizeof(int32_t));
+		if (xdata == NULL)
+			return EXIT_FAILURE;
+		parse_rtdata("RT_datastream.txt", &xdata, i+1);
 
 		while(hps_write_new_batch == 0){
 			hps_write_new_batch = 1 & read_accelerator(1);
 		}
 		if (timesteps != 9)
 		{
-			parse_rtdata("RT_datastream.txt", &xdata, i+1);
 
 			write_accelerator(0, 0); //switch back to HPS mode
 			xocram_fill_RT(xocram, xdata);
 			write_accelerator(0, 2); // allow accelerator to write to XOCRAM B port
 		}
-
+		free(xdata);
 		while(hps_DEBUG_read == 0){
 			hps_DEBUG_read = (2 & read_accelerator(1) >> 1);
 		}
 
 		write_accelerator(0, 0); //switch back to HPS mode
+	    DEBUG_data_words = calloc(26*20, sizeof(int32_t));
+	    if (DEBUG_data_words == NULL)
+	    	return EXIT_FAILURE;
 		read_xocram(1, (int32_t*)xocram, DEBUG_data_words);
-
+		 DEBUG_data_maxp = calloc(1078, sizeof(int32_t));
+		if (DEBUG_data_maxp == NULL)
+			return EXIT_FAILURE;
 		get_data_maxp(DEBUG_data_maxp, DEBUG_data_words);
+		free(DEBUG_data_maxp);
+		DEBUG_data_gru = calloc(400, sizeof(int32_t));
+		if (DEBUG_data_gru == NULL)
+			return EXIT_FAILURE;
 		get_data_gru(DEBUG_data_gru, DEBUG_data_words+20*22);
-
+		free(DEBUG_data_gru);
+		free(DEBUG_data_words);
 		snprintf(filename, "res_acc/MAXP_t%c.txt", (char) timesteps);
 		res_file = fopen(filename, "w");
 		if (!res_file)
@@ -112,10 +130,10 @@ int main() {
 		}
 		fclose(res_file);
     }
-	free(xdata);
-	free(DEBUG_data_gru);
-	free(DEBUG_data_maxp);
-	free(DEBUG_data_words);
+	//free(xdata);
+	//free(DEBUG_data_gru);
+	//free(DEBUG_data_maxp);
+	//free(DEBUG_data_words);
     munmap_peripherals();
     close_physical_memory_device();
     return 0;
